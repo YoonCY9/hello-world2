a2796a819df4995896df5b1e376527e9df7b7b55
TJOEUNIS <fana9088@naver.com>
2024-11-11 16:54:21 +0900

    Update Main.java

--- src/Variable.java
+++ src/Variable.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Variable {
 
     public static void main(String[] args) {
@@ -16,6 +18,20 @@ public class Variable {
         System.out.println("국적: " + nationality);
         System.out.println("키: " + height);
 
-
+        LocalDate 개봉일 = LocalDate.parse("2024-10-01");
+        System.out.println(개봉일);
+        
+        int 연봉 = 5400;
+        int 월급 = 연봉 / 13;
+        System.out.println("월급 = " + 월급);
+        월급 = 연봉 / 12;
+        System.out.println("월급 = " + 월급);
+        
+        int 상품구매수량 = 1;
+        상품구매수량 += 1;
+        상품구매수량 += 1;
+        상품구매수량 += 1;
+        상품구매수량 += 1;
+        System.out.println("상품구매수량 = " + 상품구매수량);
     }
 }
