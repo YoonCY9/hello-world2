4f9352b8201e880bf2f83f8c57792e7907912f8c
TJOEUNIS <fana9088@naver.com>
2024-11-11 16:09:58 +0900

    Update Main.java

---
+++ src/Operation.java
@@ -0,0 +1,21 @@
+public class Operation {
+
+    public static void main(String[] args) {
+        // 산술 연산
+        System.out.println(0 % 3);
+        System.out.println(1 % 3);
+        System.out.println(2 % 3);
+        System.out.println(3 % 3);
+        System.out.println(4 % 3);
+        System.out.println(5 % 3);
+        System.out.println(6 % 3);
+        System.out.println(7 % 3);
+        System.out.println(8 % 3);
+
+        //비교 연산
+        System.out.println(3 == 2);
+
+
+
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+
+    public static void main(String[] args) {
+
+        int student = 20;
+        System.out.println("수강생 수는 " + student + "명입니다.");
+        student = 25;
+        System.out.println("수강생 수는 " + student + "명입니다.");
+
+        System.out.println();
+
+        String name = "박효신";
+        String nationality = "대한민국";
+        int height = 178;
+        System.out.println("이름: " + name);
+        System.out.println("국적: " + nationality);
+        System.out.println("키: " + height);
+
+
+    }
+}
